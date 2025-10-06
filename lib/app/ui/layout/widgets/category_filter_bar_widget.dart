284621edaea45904048792c6184334557d2944e3
WilliamsAdm <i2310607@continental.edu.pe>
2025-10-05 23:22:33 -0500

    rollback

--- lib/app/ui/layout/widgets/category_filter_bar_widget.dart
+++ lib/app/ui/layout/widgets/category_filter_bar_widget.dart
@@ -1,81 +1,85 @@
-import 'package:anttec_mobile/services/category_services.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:anttec_mobile/core/constants/categories.dart';
 import 'package:anttec_mobile/core/styles/colors.dart';
 import 'package:anttec_mobile/core/styles/texts.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CategoryFilterBarWidget extends StatefulWidget {
+class CategoryFilterBarDesign extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onSelected;
+
+  const CategoryFilterBarDesign({
+    super.key,
+    required this.selectedIndex,
+    required this.onSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10.0),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: List.generate(categories.length, (index) {
+            final bool isSelected = index == selectedIndex;
+            return Padding(
+              padding: const EdgeInsets.only(right: 14.0),
+              child: ChipTheme(
+                data: ChipTheme.of(context).copyWith(
+                  showCheckmark: false,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ), // Cambia aquí el radio
+                  ),
+                ),
+                child: ChoiceChip(
+                  label: Text(categories[index]),
+                  selected: isSelected,
+                  onSelected: (_) => onSelected(index),
+                  selectedColor: AppColors.secondaryP,
+                  backgroundColor: AppColors.secondaryS,
+                  side: BorderSide(color: Colors.transparent),
+                  labelStyle: AppTexts.body2M.copyWith(
+                    color: isSelected ? AppColors.primaryS : AppColors.darkT,
+                  ),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryFilterBarWidget extends StatelessWidget {
   final String selectedCategory;
+
   const CategoryFilterBarWidget({super.key, required this.selectedCategory});
 
   @override
-  State<CategoryFilterBarWidget> createState() =>
-      _CategoryFilterBarWidgetState();
-}
-
-class _CategoryFilterBarWidgetState extends State<CategoryFilterBarWidget> {
-  int selectedIndex = 0;
-  List<String> _categories = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadCategories();
-  }
-
-  void _loadCategories() async {
-    final fetched = await CategoryService.fetchCategories();
-    setState(() {
-      _categories = ['Todos', ...fetched];
-      selectedIndex = _getInitialIndex(widget.selectedCategory);
-    });
-  }
-
-  int _getInitialIndex(String category) {
-    final index = _categories.indexWhere(
-      (c) => c.toLowerCase() == category.toLowerCase(),
+  Widget build(BuildContext context) {
+    return CategoryFilterBarDesign(
+      selectedIndex: _getCategoryIndex(),
+      onSelected: (int index) => _onCategorySelected(context, index),
     );
-    return index >= 0 ? index : 0;
   }
 
-  void _onCategorySelected(int index) {
-    setState(() => selectedIndex = index);
-    final selected = _categories[index].toLowerCase();
+  int _getCategoryIndex() {
+    final index = categories.indexWhere(
+      (c) => c.toLowerCase() == selectedCategory.toLowerCase(),
+    );
+    return index < 0 ? 0 : index;
+  }
+
+  void _onCategorySelected(BuildContext context, int index) {
+    final selected = categories[index].toLowerCase();
     if (selected == 'todos') {
       context.goNamed('home');
     } else {
       context.goNamed('home-category', pathParameters: {'category': selected});
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_categories.isEmpty) {
-      return const Center(child: CircularProgressIndicator());
-    }
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(_categories.length, (index) {
-          final isSelected = index == selectedIndex;
-          return Padding(
-            padding: const EdgeInsets.only(right: 12.0),
-            child: ChoiceChip(
-              label: Text(
-                _categories[index],
-                style: AppTexts.body2M.copyWith(
-                  color: isSelected ? AppColors.primaryS : AppColors.darkT,
-                ),
-              ),
-              selected: isSelected,
-              onSelected: (_) => _onCategorySelected(index),
-              selectedColor: AppColors.secondaryP,
-              backgroundColor: AppColors.secondaryS,
-            ),
-          );
-        }),
-      ),
-    );
   }
 }
